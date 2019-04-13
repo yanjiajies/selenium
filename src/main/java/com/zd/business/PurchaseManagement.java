78872373b3d5a9b0d016048b1587e6e34f58551f
root <1302711300@qq.com>
2019-04-13 14:41:09 +0800

    Allure报错自动截图
    Signed-off-by: root <1302711300@qq.com>

--- src/main/java/com/zd/business/PurchaseManagement.java
+++ src/main/java/com/zd/business/PurchaseManagement.java
@@ -57,7 +57,7 @@ public class PurchaseManagement extends AbstractTestNGSpringContextTests {
     public void Login(){
         loginOP.toLogin("admin", "edeming123");
         Verify.assertEquals(driver.getTitle(), "首页", "登录校验失败");
-        //homeOP.menuClick("采购管理系统");
+        homeOP.menuClick("采购管理系统");
     }
     //采购类型数据驱动
     @DataProvider(name = "purchaseType")
