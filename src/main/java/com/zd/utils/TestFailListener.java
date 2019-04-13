b1afb26ff80a8e2d74ac80b4c0e998d2b0dd80bb
root <1302711300@qq.com>
2019-04-13 15:08:44 +0800

    Allure报错自动截图
    Signed-off-by: root <1302711300@qq.com>

--- src/main/java/com/zd/utils/TestFailListener.java
+++ src/main/java/com/zd/utils/TestFailListener.java
@@ -1,23 +1,36 @@
 package com.zd.utils;
 
+import com.google.common.io.Files;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.IHookCallBack;
+import org.testng.IHookable;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
-public class TestFailListener extends TestListenerAdapter {
+import java.io.File;
+import java.io.IOException;
+
+public class TestFailListener implements IHookable {
     public static WebDriver driver;
-    @Override
-    public void onTestFailure(ITestResult result) {
-        takePhoto();
+    public void run(IHookCallBack callBack, ITestResult testResult) {
+
+        callBack.runTestMethod(testResult);
+        if (testResult.getThrowable() != null) {
+            try {
+                takeScreenShot(testResult.getMethod().getMethodName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    @Attachment(value = "screen shot",type = "image/png")
-    public byte[]  takePhoto(){
-        byte[] screenshotAs = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
-        return screenshotAs;
-    }
 
+    @Attachment(value = "Failure in method {0}", type = "image/png")
+    private byte[] takeScreenShot(String methodName) throws IOException {
+
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+    }
 }
