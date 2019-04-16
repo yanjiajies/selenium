7cf7d754334434047bf15408f23182448704f344
root <1302711300@qq.com>
2019-04-16 14:46:23 +0800

    Allure报错自动截图
    Signed-off-by: root <1302711300@qq.com>

--- src/main/java/com/zd/business/PurchaseManagement.java
+++ src/main/java/com/zd/business/PurchaseManagement.java
@@ -21,7 +21,7 @@ import java.util.HashMap;
 /**
  * 采购管理系统用例
  */
-@Listeners({TestFailListener.class})
+@Listeners
 @ContextConfiguration({"classpath*:/spring-test-config.xml"})
 public class PurchaseManagement extends AbstractTestNGSpringContextTests {
     @Autowired
@@ -55,10 +55,9 @@ public class PurchaseManagement extends AbstractTestNGSpringContextTests {
      */
     @Test(description = "Login",priority = 0)
     public void Login(){
-        loginOP.toLogin("admin", "123");
+        loginOP.toLogin("admin", "edeming123");
         //Verify.assertEquals(driver.getTitle(), "首页", "登录校验失败");
-        Assert.assertEquals("2222222222","22222222222222");
-        homeOP.menuClick("123");
+        homeOP.menuClick("采购管理系统");
     }
     //采购类型数据驱动
     @DataProvider(name = "purchaseType")
--- src/main/java/com/zd/utils/TestFailListener.java
+++ src/main/java/com/zd/utils/TestFailListener.java
@@ -1,28 +1,26 @@
 package com.zd.utils;
 
-import com.google.common.io.Files;
+
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.IHookCallBack;
-import org.testng.IHookable;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
 
 
-                public class TestFailListener extends TestListenerAdapter {
-                    public static WebDriver driver;
-                    @Override
-                    public void onTestFailure(ITestResult result) {
-                        takePhoto();
-                    }
+    public class TestFailListener extends TestListenerAdapter {
+        public  static WebDriver driver;
+        @Override
+        public void onTestFailure(ITestResult result) {
+            takePhoto();
+        }
 
-                    @Attachment(value = "screen shot",type = "image/png")
-                    public byte[]  takePhoto(){
-                        byte[] screenshotAs = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
-                        return screenshotAs;
-                    }
+        @Attachment(value = "screen shot",type = "image/png")
+        public byte[]  takePhoto(){
+            byte[] screenshotAs = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+            return screenshotAs;
+        }
 
-                }
+    }
